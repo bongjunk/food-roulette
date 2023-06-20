3dacf0c61099118863225e93a406620281a0c2d7
bongjunk <mydlwm_s@naver.com>
2023-06-20 16:27:50 +0900

    yml 추가

--- config/config.ts
+++
@@ -1,4 +0,0 @@
-export const prefix =
-  process.env.NODE_ENV === 'production'
-    ? 'https://bongjunk.github.io/food-roulette'
-    : '';
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Page from './components/home';
+import Page from './roulette/home';
 
 const Home = () => {
   return (

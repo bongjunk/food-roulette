6bfcd5c846e2fee5e205ed4ed36c63067dd1f818
bongjunk <mydlwm_s@naver.com>
2023-06-20 10:26:50 +0900

    룰렛 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Wheel } from 'react-custom-roulette';
+
+interface StyleType {
+  backgroundColor?: string;
+  textColor?: string;
+}
+
+interface WheelData {
+  id?: number;
+  options?: string;
+  style?: StyleType;
+}
 
 export default function Home() {
   const [mustSpin, setMustSpun] = useState<boolean>(false);
   const [prizeNumber, setPrizeNumber] = useState<number>(0);
+  // const [init, setInit] = useState<boolean>(false);
 
-  const data = [
+  const data: WheelData = [
     {
       id: 1,
       option: '냉면',
@@ -35,7 +47,7 @@ export default function Home() {
     },
     {
       id: 6,
-      option: '뜸들이다',
+      option: '덮밥',
       style: { backgroundColor: '#60caeb', textColor: 'black' },
     },
     {
@@ -93,13 +105,19 @@ export default function Home() {
     }
   };
 
-  console.log('prizeNumber', prizeNumber);
+  useEffect(() => {
+    setTimeout(() => {
+      !mustSpin && prizeNumber
+        ? alert(`추첨결과는 ${data[prizeNumber]?.option} 입니다!`)
+        : null;
+    }, 500);
+  }, [!mustSpin]);
 
   return (
     <>
       <article className="flex justify-center text-center">
         <div>
-          <p>rendom_food</p>
+          <p>점심메뉴 선정 v0.1.0</p>
           <Wheel
             mustStartSpinning={mustSpin}
             prizeNumber={prizeNumber}
@@ -111,10 +129,17 @@ export default function Home() {
               setMustSpun(false);
             }}
           />
-          <button onClick={() => handleSpinClick()} className="text-center">
+          <button
+            onClick={() => handleSpinClick()}
+            className="text-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          >
             돌리기
           </button>
-          <p>{!mustSpin ? data[prizeNumber]?.option : '추첨결과'}</p>
+          <p>
+            {!mustSpin && prizeNumber
+              ? `오늘의 점심메뉴는 ${data[prizeNumber]?.option} 당첨!`
+              : '오늘의 점심'}
+          </p>
         </div>
       </article>
     </>
